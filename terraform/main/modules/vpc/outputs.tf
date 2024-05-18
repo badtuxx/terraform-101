db3477fdf901eb6683281e1321a7493e4710ef64
Raphael Viana <123553646+rnvdev@users.noreply.github.com>
2024-05-18 15:57:58 -0300

    Update outputs.tf
    Insert ARN on output.tf

--- terraform/main/modules/vpc/outputs.tf
+++ terraform/main/modules/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
   value       = aws_vpc.giropops_vpc.cidr_block
 }
+
+output "arn" {
+  description = "ARN of vpc"
+  value       = aws_vpc.giropops_vpc.arn
+}
