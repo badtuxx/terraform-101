c22265e9798924a6dfb912c048e822da7b4d9622
Jeferson Fernando <jfnredes@gmail.com>
2024-05-18 17:29:16 +0200

    testando lint

---
+++ terraform/main/modules/security_group/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.4.0"  # Substitua pela versão mínima que você precisa
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Substitua pela versão do provedor que você deseja fixar
+    }
+  }
+}
