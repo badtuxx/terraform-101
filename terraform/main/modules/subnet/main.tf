5345d32abc55216d6455fa28a58ce7035fb932b8
Antonio <antonio.medeiros@estudante.ufjf.br>
2024-05-18 15:56:50 -0300

    add subnet var

--- terraform/main/modules/subnet/main.tf
+++ terraform/main/modules/subnet/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = var.vpc_id # Usa a variável passada
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.public_cidr_block
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-1a"
 
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet" {
 # modules/subnet/main.tf
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = var.vpc_id # Usa a variável passada
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = var.private_cidr_block
   map_public_ip_on_launch = false
   availability_zone       = "eu-west-1b"
 
--- terraform/main/modules/subnet/variables.tf
+++ terraform/main/modules/subnet/variables.tf
@@ -2,3 +2,15 @@ variable "vpc_id" {
   description = "The VPC ID where subnets will be created"
   type        = string
 }
+
+variable "public_cidr_block" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default = "10.0.1.0/24"
+}
+
+variable "private_cidr_block" {
+  description = "The CIDR block for the private subnet"
+  type        = string
+  default = "10.0.1.0/24"
+}
