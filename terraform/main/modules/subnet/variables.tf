04c8eaa1f41203eebf85b39596b3bd717dae312d
Antonio <antonio.medeiros@estudante.ufjf.br>
2024-05-18 15:58:47 -0300

    fix

--- terraform/main/modules/subnet/variables.tf
+++ terraform/main/modules/subnet/variables.tf
@@ -12,5 +12,5 @@ variable "public_cidr_block" {
 variable "private_cidr_block" {
   description = "The CIDR block for the private subnet"
   type        = string
-  default = "10.0.1.0/24"
+  default = "10.0.2.0/24"
 }
