12cdc149c17431c47ea4f43424b5344f39e1831d
Jeferson Fernando <jfnredes@gmail.com>
2024-05-18 17:32:56 +0200

    testando lint

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,4 +1,14 @@
-# main.tf
+terraform {
+  required_version = ">= 1.4.0"  # Substitua pela versão mínima que você precisa
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Substitua pela versão do provedor que você deseja fixar
+    }
+  }
+}
+
 module "vpc" {
   source = "./modules/vpc"
 }
